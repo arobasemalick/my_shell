f05576d23ac333cf17d3e711b581af959759938a
arobasemalick <elhadjimalicklo001@gmail.com>
2024-12-21 15:09:37 +0100

    Headers

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char* argv[]){ //the fonction that calls the shell
+
+    my_shell();
+
+    return 0;
+}
---
+++ src/my_shell.h
@@ -0,0 +1,9 @@
+#ifndef __MY_SHELL_H__
+#define __MY_SHELL_H__
+
+//LIBRARIES
+#include <stdio.h>
+#include <unistd.h>
+
+
+#endif
