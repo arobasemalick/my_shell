4ace73d0a7c8c1b3164c2343444fb47d19f12438
arobasemalick <elhadjimalicklo001@gmail.com>
2024-12-23 17:26:30 +0100

    Handling EOF

--- src/reading.c
+++ src/reading.c
@@ -6,9 +6,14 @@ char* reading(void){
     size_t n=0;
 
     if (getline(&buf,&n,stdin)==-1){
+        if (feof(stdin)) {  
+        exit(EXIT_SUCCESS);  // We recieved an EOF
+        }
+        else{
         free(buf);
         perror("my_shell : error on reading\n");
         exit(EXIT_FAILURE);
+        }
     }
     
     return buf;
