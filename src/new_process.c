290450c9564df998509fda8d024330eba5f51c82
arobasemalick <elhadjimalicklo001@gmail.com>
2024-12-23 16:04:27 +0100

    Adding comments

--- src/new_process.c
+++ src/new_process.c
@@ -6,19 +6,19 @@ int new_process(char **args){
 
     pid=fork();   //create a child process
 
-    if (pid==0){
+    if (pid==0){  // on the child process
         if (execvp(args[0], args) == -1)
             {
                 perror("my_shell : error on child process in new_process");
             }
         exit(EXIT_FAILURE);
     }
-    else if (pid<0){
+    else if (pid<0){  // error handling
         perror("my_shell : error on fork in new_process");
     }
-    else {
+    else {  //parent process
         do {
-            waitpid(pid, &status, WUNTRACED);
+            waitpid(pid, &status, WUNTRACED);  //waiting for the child process to change status
         }
         while (!WIFEXITED(status) && !WIFSIGNALED(status));        
     }
